04b13f6b7946b2414dc4fb1e2acabd8eaaf95064
Simon Hudson <simon.hudson@carterdigital.com.au>
2016-09-28 18:02:40 +1000

    Working attice energies and magnetisations

--- proj_4.c
+++ proj_4.c
@@ -11,8 +11,14 @@
 #define EXIT_SUCCESS 0
 #endif
 
-#define MAX_SIZE 10000000
-#define ARRAY_SIZE 10000000
+#define MAX_SWEEPS 1000
+#define MEASUREMENT_SWEEP_SEPARATION 10
+#define X_SIZE 50
+#define Y_SIZE 50
+
+double boltzmann_const = 1.38064852e-23;
+// critical temperature for phase change
+double beta_sub_c = 0.4407;
 
 //
 // create 1 D array of char in memory
@@ -22,6 +28,24 @@ char * create_1d_array_c(int cols)
     char * p1dArray = malloc(cols*sizeof(char));
     return p1dArray;
 }
+
+//
+// create 1 D array of int in memory
+//
+int * create_1d_array_i(int cols)
+{
+    int * p1dArray = malloc(cols*sizeof(int));
+    return p1dArray;
+}
+
+//
+// Destroy a created 1D array of int
+//
+void destroy_1d_array_i(int p1dArray[])
+{
+    free(p1dArray);
+}
+
 //
 // Destroy a created 1D array of char
 //
@@ -59,6 +83,35 @@ void destroy_2d_array_c(char *array[], int rows)
     free(array);
 }
 
+//
+// Create an array of dimensions (rows,cols) of int in memory
+//
+int ** create_2d_array_i(int rows, int cols)
+{
+    int ** p2dArray;
+    p2dArray = (int **) malloc(rows*sizeof(char *));
+    int i = 0;
+    for(i=0; i< rows; i++)
+    {
+        p2dArray[i] = (int *) malloc(cols*sizeof(int));
+    }
+    return p2dArray;
+}
+
+
+//
+// Destroy a 2D array of dimensions (rows,[]) of int in memory
+//
+void destroy_2d_array_i(int *array[], int rows)
+{
+    int i=0;
+    for(i=0;i<rows;i++)
+    {
+        free(array[i]);
+    }
+    free(array);
+}
+
 void print_array_c(char ** array, int rows, int cols)
 {
     int i,j;
@@ -66,13 +119,15 @@ void print_array_c(char ** array, int rows, int cols)
     {
         for(j=0;j<cols;j++)
         {
+//            printf("x: %d, y: %d\n", i, j);
             printf(" %c", array[i][j]);
         }
         printf("\n");
     }
 }
 
-void display_scatter_char(char ** array, int rows, int cols, int pos_incr, char * heading, char *x_label, char * y_label)
+void display_scatter_arr_char(char **array, int rows, int cols, int pos_incr, char *heading, char *x_label,
+                              char *y_label)
 {
     static float fxp[1];
     static float fyp[1];
@@ -99,7 +154,7 @@ void display_scatter_char(char ** array, int rows, int cols, int pos_incr, char 
         fyp[0] = (float)y_pos;
 
         for(k=0; k < cols; k++) {
-            if ('u' == array[j][k]) {
+            if (array[j][k] == 'u') {
                 char_code = 30;
             } else {
                 char_code = 31;
@@ -124,52 +179,285 @@ void display_scatter_char(char ** array, int rows, int cols, int pos_incr, char 
 
 }
 
-char interr_array(char ** array, int x_pos, int y_pos)
+char query_array(char **array, int x_pos, int y_pos, int x_size, int y_size)
 {
+    // first deal with out-of-bounds indices
+    if (x_pos > x_size - 1) {
+        x_pos = 0;
+    }
+
+    if (x_pos < 0) {
+        x_pos = x_size - 1;
+    }
+
+    if (y_pos > y_size - 1) {
+        y_pos = 0;
+    }
+
+    if (y_pos < 0) {
+        y_pos = y_size - 1;
+    }
+
     return array[x_pos][y_pos];
+}
+
+int get_spin_val_from_char(char this_spin_char)
+{
+    int val = this_spin_char == 'u' ? 1 : -1;
+//    printf("Val: %d\n", val);
+    return val;
+}
+
+double calc_delta_e(char **array, int x_pos, int y_pos, int x_size, int y_size)
+{
+    double val;
+
+    char this_spin_char = query_array(array, x_pos, y_pos, x_size, y_size);
+    char x_minus_one_neigh = query_array(array, x_pos - 1, y_pos, x_size, y_size);
+    char x_plus_one_neigh = query_array(array, x_pos + 1, y_pos, x_size, y_size);
+    char y_minus_one_neigh = query_array(array, x_pos, y_pos - 1, x_size, y_size);
+    char y_plus_one_neigh = query_array(array, x_pos, y_pos + 1, x_size, y_size);
+    val = -2.0 * get_spin_val_from_char(this_spin_char) * (get_spin_val_from_char(x_minus_one_neigh) + get_spin_val_from_char(x_plus_one_neigh) + get_spin_val_from_char(y_minus_one_neigh) + get_spin_val_from_char(y_plus_one_neigh));
+    return val;
+}
+
+void sweep_2d_array(char **array, int x_size, int y_size, int *knuth_arr, int knuth_size, double beta)
+{
+    int x_pos, y_pos, k_count, this_k_val;
+    double this_delta_e;
+
+    for (k_count = 0; k_count < knuth_size; k_count++) {
+        this_k_val = knuth_arr[k_count];
+        x_pos = this_k_val % x_size;
+        y_pos = (int)floor((double)(this_k_val / x_size));
+
+        // flip spin
+        if (array[x_pos][y_pos] == 'u') {
+            array[x_pos][y_pos] = 'd';
+        } else {
+            array[x_pos][y_pos] = 'u';
+        }
+
+        this_delta_e = calc_delta_e(array, x_pos, y_pos, x_size, y_size);
+
+//        printf("This delta_e: %lf\n", this_delta_e);
+
+        if (!(this_delta_e < 0.0 || exp(-beta * this_delta_e) > drand48())) {
+            // flip spin back
+            if (array[x_pos][y_pos] == 'u') {
+                array[x_pos][y_pos] = 'd';
+            } else {
+                array[x_pos][y_pos] = 'u';
+            }
+        }
+    }
+}
+
+void fill_2d_array_c(char ** array, int x_size, int y_size, char * start)
+{
+    int j, k, boundary;
+    // set default value for cold start
+    char spin = 'u';
+
+    for (j = 0; j < x_size; j++)
+    {
+
+        for(k = 0; k < y_size; k++)
+        {
+            boundary = 0;
+
+            if (j == x_size - 1)
+            {
+                spin = array[0][k];
+                boundary = 1;
+            }
+
+            if (j == x_size - 2)
+            {
+                spin = array[1][k];
+                boundary = 1;
+            }
+
+            if (k == y_size - 1)
+            {
+                spin = array[j][0];
+                boundary = 1;
+            }
+
+            if (k == y_size - 2)
+            {
+                spin = array[j][1];
+                boundary = 1;
+            }
+
+            if (boundary == 0) {
+                // do this if we're not at a boundary
+                if (start = "hot") {
+                    // randomise
+                    if (drand48() > 0.5) {
+                        spin = 'u';
+                    } else {
+                        spin = 'd';
+                    }
+                }
+            }
+
+            array[j][k] = spin;
+        }
+    }
+}
+
+double calc_mag_site(char **array, int x_pos, int y_pos)
+{
+    if (array[x_pos][y_pos] == 'u') {
+        return 1.0;
+    } else {
+        return 0.0;
+    }
+}
+
+double calc_mag_of_lattice(char **array, int x_size, int y_size)
+{
+    double magnetisation = 0.0;
+    int j, k;
+
+    for (j = 0; j < x_size; j++) {
+        for (k = 0; k < y_size; k++) {
+            magnetisation = magnetisation + calc_mag_site(array, j, k);
+        }
+    }
+
+    return magnetisation;
+}
+
+void fill_knuth_1d(int *knuth_arr, int size, int *counter)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+    {
+        knuth_arr[i] = *counter;
+        * counter = * counter + 1;
+    }
+}
+
+void print_array_knuth(int * array, int size)
+{
+    int i;
+    for(i=0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    printf("\n");
+}
+
+void swap_elements_one_d_arr(int one_d_array[], int pos_1, int pos_2)
+{
+    int temp = one_d_array[pos_1];
+    one_d_array[pos_1] = one_d_array[pos_2];
+    one_d_array[pos_2] = temp;
+}
+
+void knuth(int one_d_array[], int size)
+{
+    int i, j;
+    double boundary;
+    int found;
+
+    for (i = 0; i < size; i++) {
+        boundary = (double)i + drand48() * (double)(size - i - 1);
+        found = 0;
+        for (j = i + 1; j < size && 0 == found; j++) {
+            if (boundary < (double)j) {
+//                printf("knuth i: %d, j: %d\n", i, j);
+                swap_elements_one_d_arr(one_d_array, i, j);
+                found = 1;
+            }
+        }
+    }
+}
+
+double calc_energy_of_lattice(char ** Array, int x_size, int y_size)
+{
+    double energy = 0.0;
+    int j, k;
+
+    for (j = 0; j < x_size; j++) {
+        for (k = 0; k < y_size; k++) {
+            energy = energy + calc_delta_e(Array, j, k, x_size, y_size);
+        }
+    }
+
+    return energy;
 }
 
 int main(void)
 {
     int j, k;
-    int xsize = 25;
-    int ysize = 50;
     char spin;
     srand48((int) time(NULL));
 
-    // Allocate a region of memory of size xsize*ysize*sizeof(double) and return a pointer to it
-    char ** Array = create_2d_array_c(xsize, ysize);
-    for(j=0; j < xsize; j++)
-    {
-        for(k=0; k < ysize; k++)
-        {
-            if (drand48() > 0.5) {
-                spin = 'u';
-            } else {
-                spin = 'd';
-            }
-            Array[j][k] = spin;
-        }
-    }
+    // Allocate a region of memory of size x_size*Y_SIZE*sizeof(double) and return a pointer to it
+    char ** Array = create_2d_array_c(X_SIZE, Y_SIZE);
+    fill_2d_array_c(Array, X_SIZE, Y_SIZE, "hot");
 
-//    print_array_c(Array, xsize, ysize);
+    print_array_c(Array, X_SIZE, Y_SIZE);
 
-    // Initiate cpgplot and open output device
-
-//    if (1 != cpgbeg(0, "?", 1, 1))
-    if (1 != cpgbeg(0, "/XWINDOW", 1, 1))
-//    if (1 != cpgbeg(0, "proj_3_plot.ps/CPS", 1, 1))
+    /*
+//    if (cpgbeg(0, "?", 1, 1) != 1)
+    if (cpgbeg(0, "/XWINDOW", 1, 1) != 1)
+//    if (cpgbeg(0, "proj_3_plot.ps/CPS", 1, 1) != 1)
     {
         exit(EXIT_FAILURE);
     }
     cpgask(1);
+    */
 
-    display_scatter_char(Array, xsize, ysize, 1, "Plotting a 2-D array of char representing spins", "x-axis", "y-axis");
+    int i;
+    int lattice_size = X_SIZE * Y_SIZE;
+    int knuth_arr_size, sweep_counter;
+    double lowest_energy_of_lattice = 0.0;
+    double beta, magnetisation, energy;
+    int * knuth_arr = create_1d_array_i(lattice_size);
 
-    cpgend();
+//    display_scatter_arr_char(Array, X_SIZE, Y_SIZE, 1, "Plotting a 2-D array of char representing spins", "x-axis", "y-axis");
 
-    // find 4,7th element of array
-    printf("The 4, 7th element of array: %c", interr_array(Array, 4, 7));
+//    printf("Table\n\n");
+//    printf("beta\t\tmagnetisation\n");
 
-    destroy_2d_array_c(Array, xsize); // deallocate the memory
+    knuth_arr_size = 0;
+    fill_knuth_1d(knuth_arr, lattice_size, &knuth_arr_size);
+
+    for (beta = 0.1; beta <= 10; beta = beta + 0.1) {
+        for (sweep_counter = 0; sweep_counter < MAX_SWEEPS; sweep_counter++) {
+            knuth(knuth_arr, knuth_arr_size);
+//            print_array_knuth(knuth_arr, knuth_arr_size);
+
+            sweep_2d_array(Array, X_SIZE, Y_SIZE, knuth_arr, knuth_arr_size, beta);
+//        printf("Flipped element of array at x = %d, y = %d: %c\n", x_rand_i, y_rand_i, query_array(Array, x_rand_i, y_rand_i, X_SIZE, Y_SIZE));
+
+            if (sweep_counter % MEASUREMENT_SWEEP_SEPARATION == 0) {
+                // take a measurement
+//                magnetisation = calc_mag_of_lattice(Array, X_SIZE, Y_SIZE);
+                energy = calc_energy_of_lattice(Array, X_SIZE, Y_SIZE);
+
+//                printf("Energy after %d sweeps: %lf\n", sweep_counter, energy);
+//                printf("Magnetisation after %d sweeps: %lf\n", sweep_counter, magnetisation);
+            }
+        }
+
+        printf("Energy after %d sweeps: %lf\n", sweep_counter, calc_energy_of_lattice(Array, X_SIZE, Y_SIZE));
+        printf("Magnetisation after %d sweeps: %lf\n", sweep_counter, calc_mag_of_lattice(Array, X_SIZE, Y_SIZE));
+        printf("\n");
+    }
+
+//    display_scatter_arr_char(Array, X_SIZE, Y_SIZE, 1, "Plotting a 2-D array of char representing spins", "x-axis", "y-axis");
+
+//    cpgend();
+
+    destroy_1d_array_i(knuth_arr);
+    destroy_2d_array_c(Array, X_SIZE); // deallocate the memory
+
+//    printf("Critical temperature for phase change, T_sub_c: %.6E\n", 1.0 / boltzmann_const * beta_sub_c);
 }
